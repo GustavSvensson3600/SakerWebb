9c1a112d13826a9136264d7a8a531c5d37dd5b16
Gustav Svensson <Gustav Svensson>
2016-09-22 13:36:55 +0200

    Signed-off-by: Gustav Svensson <Gustav Svensson>

--- index.php
+++ index.php
@@ -1,30 +1,58 @@
-
-<?php
-	include('login.php'); // Includes Login Script
-
-	if(isset($_SESSION['login_user'])){
-		header("location: profile.php");
-	}
-?>
 <!DOCTYPE html>
 <html>
-	<head>
-	<title>Login</title>
-	</head>
-	<body>
-		<div id="main">
-			<h1>Login Example</h1>
-			<div id="login">
-				<h2>Login</h2>
-				<form action="" method="post">
-					<label>UserName :</label>
-					<input id="name" name="username" placeholder="username" type="text">
-					<label>Password :</label>
-					<input id="password" name="password" placeholder="password" type="password">
-					<input name="submit" type="submit" value=" Login ">
-					<span><?php echo $error; ?></span>
-				</form>
+<head>
+	<meta charset="UTF-8">
+	<title>Stuff 'N Things </title>
+	<link rel="stylesheet" href="Css.css">
+
+</head>
+
+<body>
+	<?php
+	include('login.php'); // Includes Login Script
+	if(!isset($_SESSION['login_user'])) { ?>
+	<div id="header">
+			<img class="src" src="D-logo-org-bw.png" /img>
+
+		<form style="display:inline-block; float:right" method="post">
+
+			<span>Username:</span>
+			<input id="name" name="username" type="text">
+			<span>Password:</span>
+
+			<input id="password" name="password" type="password"><br>
+			<input type="submit" value="Submit">
+		</form>
+		<h3 class="center">ZeroHelp</h3>
+	</div>
+	<?php } else { ?>
+	<?php } ?>
+
+		<main>
+			<div id="content">
+				<div class="innertube">
+				</div>
 			</div>
-		</div>
-	</body>
+
+
+		<nav id="nav">
+			<div class="innertube">
+
+				<li><a href="#">Sälj biljeter</a></li>
+				<li><a href="#">Lägg till Sittningar</a></li>
+				<li><a href="#">Lägg till Events</a></li>
+
+				<li><a href="#">Listor</a></li>
+				<li><a href="#">Matpreferencer</a></li>
+				<li><a href="#">Link 3</a></li>
+
+
+				<li><a href="#">Sittningar</a></li>
+				<li><a href="#">Sittandes</a></li>
+				<li><a href="#">Andra event</a></li>
+			</div>
+		</nav>
+		</main>
+</body>
+
 </html>
--- tables.sql
+++ tables.sql
@@ -2,8 +2,26 @@
 creating new ones. */
 
 /* drop table if exists tableName;*/
+drop table if exist Users;
+drop table if exist Items;
 
 /* Create the tables we need with the right format*/
+create table Users (
+userName varchar(100) not null,
+passWord varchar(100) not null,
+salt varchar(20) not null,
+adress varchar(100) not null,
+status int default 0,
+primary key (userName)
+);
+
+create table Item (
+itemName varchar(100) not null,
+itemNumber int auto_increment not null,
+description varchar(1000),
+price int not null,
+primary key (itemNumber)
+);
 
 
 /* Create a couple of inputs just to see if it works?
