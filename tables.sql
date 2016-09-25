d90b38defbad7c9f4238bc959f5d823f4b7321cf
JakeTheMFSnake <jakob.hakansson@outlook.com>
2016-09-26 00:16:15 +0200

    Added values without descript

--- tables.sql
+++ tables.sql
@@ -15,7 +15,7 @@ status int default 0,
 primary key (userName)
 );
 
-create table Item (
+create table Items (
 itemName varchar(100) not null,
 itemNumber int auto_increment not null,
 description varchar(1000),
@@ -26,3 +26,16 @@ primary key (itemNumber)
 
 /* Create a couple of inputs just to see if it works?
 Migh be hard if we're storing hashes. */
+
+insert into Items values ('Kayne Wests Självgodhet', 'description', 100);
+insert into Items values ('Miley Cyrus Värdighet', 'description', 100);
+insert into Items values ('Sherlock Holmes People skills', 'description', 100);
+insert into Items values ('En naturvetare med social kompetens', 'description', 100);
+insert into Items values ('En pluggande ekonom', 'description', 100);
+insert into Items values ('Trumps ryggrad', 'description', 100);
+insert into Items values ('En genuskapvetares framtid', 'description', 100);
+insert into Items values ('Hillarys hälsa', 'description', 100);
+insert into Items values ('George W Bushs IQ', 'description', 100);
+insert into Items values ('Donald Trumps löfte', 'description (gratis och finns i mängder, ej garanterad leverans)', 0);
+insert into Items values ('En genusvetenskapares drömvärld', 'description', 100);
+insert into Items values ('Dystopi fast med safe spaces.', 'description', 100);
