1b355932bae465d3fd0b2587914a08ca99f6fdd6
Mattemagikern <mansgariusson@gmail.com>
2016-09-26 10:27:27 +0200

    eddit

---
+++ CheckOut.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Stuff 'N Things </title>
+	<link rel="stylesheet" href="Css.css">
+
+</head>
+
+<body>
+	<div id="header">
+			<img class="src" src="D-logo-org-bw.png" /img>
+
+		<main>
+			<h1> Tack för ditt köp!</h1>
+			
+		</main>
+</body>
+
+</html>
