03ef70a557fecebad46cdc12a3c292c0c1a65026
JakeTheMFSnake <jakob.hakansson@outlook.com>
2016-09-27 14:28:08 +0200

    Added description and price

--- tables.sql
+++ tables.sql
@@ -27,15 +27,15 @@ primary key (itemNumber)
 /* Create a couple of inputs just to see if it works?
 Migh be hard if we're storing hashes. */
 
-insert into Items values ('Kayne Wests Självgodhet', 'description', 100);
-insert into Items values ('Miley Cyrus Värdighet', 'description', 100);
-insert into Items values ('Sherlock Holmes People skills', 'description', 100);
-insert into Items values ('En naturvetare med social kompetens', 'description', 100);
-insert into Items values ('En pluggande ekonom', 'description', 100);
-insert into Items values ('Trumps ryggrad', 'description', 100);
-insert into Items values ('En genuskapvetares framtid', 'description', 100);
-insert into Items values ('Hillarys hälsa', 'description', 100);
-insert into Items values ('George W Bushs IQ', 'description', 100);
-insert into Items values ('Donald Trumps löfte', 'description (gratis och finns i mängder, ej garanterad leverans)', 0);
-insert into Items values ('En genusvetenskapares drömvärld', 'description', 100);
-insert into Items values ('Dystopi fast med safe spaces.', 'description', 100);
+insert into Items (itemName, description, price) values ('Kayne Wests Självgodhet', 'Har du någonsin känt att du hade viljat älska dig själv mer än någoting annat? Då är detta en produkt för dig!', 1337);
+insert into Items (itemName, description, price)(itemName, description, price) values ('Miley Cyrus Värdighet', 'Den må vara liten eller knappt existerande, men det går ryckten om att den en gång i tiden betyde något.', 80085);
+insert into Items (itemName, description, price) values ('Sherlock Holmes People skills', 'Utmärkt för den som vill komplettera den där detektiv imagen.', 199);
+insert into Items (itemName, description, price) values ('En naturvetare med social kompetens', 'Varför prata och glädjas med andra när man kan har minst lika kul för sig själv? Eller?', 5);
+insert into Items (itemName, description, price) values ('En pluggande ekonom', 'Den mest sällsynta ekonomen av dem alla. Kommer endast ett få antal per nytt antag.', 875999);
+insert into Items (itemName, description, price) values ('Trumps ryggrad', 'Minimal, får plats i en fika, lätt att ta med och väger lite. Gjord av 50% besvikelse och 50% lögner.', 1);
+insert into Items (itemName, description, price) values ('En genuskapvetares framtid', 'Den må inte leda till något stort, eller något alls för den delen. Men det är en framtid och jag fick faktiskt plugga det jag verkligen tycker är viktigt!', 5);
+insert into Items (itemName, description, price) values ('Hillarys hälsa', 'Beskrivning saknas, ingen vet riktigt nu för tiden.', 1000);
+insert into Items (itemName, description, price) values ('George W Bushs IQ', 'IQ-score 91 där 100 är avarage. Inget att hänga i julgrannen men det går att göra om du vill.', 50);
+insert into Items (itemName, description, price) values ('Donald Trumps löfte', 'FYND! Hela lagret måste tömmas! Finns i mängder! Varken vi eller Dolad själv kan garanterad leverans.)', 0);
+insert into Items (itemName, description, price) values ('En genusvetenskapares drömvärld', 'För abstrakt för att kunna beskrivas. Men det är mycket rosa i den och Gudrun Schyman sägs spela en stor del i det hela.', 1234);
+insert into Items (itemName, description, price) values ('Dystopi fast med safe spaces.', 'Man skulle kunna tro att detta inte finns! Men jodå, forskare från Nordöstra Syd Sahara har kommit fram med en lösning just för det här!', 19000);
