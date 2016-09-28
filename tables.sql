441561bd4eb666afd22a70143b78ea2abe528bde
uncastablepointer <dat12dak@student.lu.se>
2016-09-28 20:39:14 +0200

    small fixes and added user insert queries

--- tables.sql
+++ tables.sql
@@ -9,7 +9,7 @@ drop table if exist Items;
 create table Users (
 userName varchar(100) not null,
 passWord varchar(256) not null,
-adress varchar(100) not null,
+address varchar(100) not null,
 status int default 0,
 primary key (userName)
 );
@@ -27,7 +27,7 @@ primary key (itemNumber)
 Migh be hard if we're storing hashes. */
 
 insert into Items (itemName, description, price) values ('Kayne Wests Självgodhet', 'Har du någonsin känt att du hade viljat älska dig själv mer än någoting annat? Då är detta en produkt för dig!', 1337);
-insert into Items (itemName, description, price)(itemName, description, price) values ('Miley Cyrus Värdighet', 'Den må vara liten eller knappt existerande, men det går ryckten om att den en gång i tiden betyde något.', 80085);
+insert into Items (itemName, description, price) values ('Miley Cyrus Värdighet', 'Den må vara liten eller knappt existerande, men det går ryckten om att den en gång i tiden betyde något.', 80085);
 insert into Items (itemName, description, price) values ('Sherlock Holmes People skills', 'Utmärkt för den som vill komplettera den där detektiv imagen.', 199);
 insert into Items (itemName, description, price) values ('En naturvetare med social kompetens', 'Varför prata och glädjas med andra när man kan har minst lika kul för sig själv? Eller?', 5);
 insert into Items (itemName, description, price) values ('En pluggande ekonom', 'Den mest sällsynta ekonomen av dem alla. Kommer endast ett få antal per nytt antag.', 875999);
@@ -38,3 +38,10 @@ insert into Items (itemName, description, price) values ('George W Bushs IQ', 'I
 insert into Items (itemName, description, price) values ('Donald Trumps löfte', 'FYND! Hela lagret måste tömmas! Finns i mängder! Varken vi eller Dolad själv kan garanterad leverans.)', 0);
 insert into Items (itemName, description, price) values ('En genusvetenskapares drömvärld', 'För abstrakt för att kunna beskrivas. Men det är mycket rosa i den och Gudrun Schyman sägs spela en stor del i det hela.', 1234);
 insert into Items (itemName, description, price) values ('Dystopi fast med safe spaces.', 'Man skulle kunna tro att detta inte finns! Men jodå, forskare från Nordöstra Syd Sahara har kommit fram med en lösning just för det här!', 19000);
+
+/* Insert some users */
+insert into Users (userName, passWord, address) values ('Sarah', '$2y$10$Jr1kd5a2Yo69V4RznPCimeY4SK/RPokNNDro5CeNPfgvNB1ZG2k.m', '95 Summit Street');
+insert into Users (userName, passWord, address) values ('Penny', '$2y$10$6gVeFMlaBzOkjr2w6oDOMO/llv1EzuTceTvtx9unsjq4TrsyQUcDi', '9828 Walnut Street');
+insert into Users (userName, passWord, address) values ('Michael', '$2y$10$FulbTBgH.VPbPsA4BPJkl..WKaONnqSmDbfqrRh80WNlmfRnQwP9G', '37 SW. Chapel Circle');
+insert into Users (userName, passWord, address) values ('Jennifer', '$2y$10$KQB5S6tvHRMpWyoQbZV/cOoJPhJpVdPYNzwHn/4PsMybaUsaswVjy', '7440 Young Lane');
+insert into Users (userName, passWord, address) values ('Algot', '$2y$10$VztHaXfus03ofJsQSyeq8.b5/D6nOwJGnSQHEoFhJvp1ebSEURRMi', '29 Ivy Avenue');
